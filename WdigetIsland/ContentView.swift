d7e1d84fb2b1f6775f807a8b51298fe26a3e0608
Sofia Swidarowicz <sofia.swidarowicz@gmail.com>
2023-08-31 09:26:56 +0200

    Initial Commit

---
+++ WdigetIsland/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WdigetIsland
+//
+//  Created by Sofia Swidarowicz on 31/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WdigetIsland/WdigetIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  WdigetIslandApp.swift
+//  WdigetIsland
+//
+//  Created by Sofia Swidarowicz on 31/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WdigetIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
